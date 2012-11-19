7028697a026367af5621418062a12b6034f85bd3
dvl <contato@xdvl.info>
2012-11-19 00:07:51 -0200

    melhorada a estrura do html no base.blade.php removendo divs desnecessarias.

--- application/views/layout/base.blade.php
+++ application/views/layout/base.blade.php
@@ -1,43 +1,38 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>CDZForever @yield('title')</title>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
-		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width">
+<head>
+	<title>CDZForever @yield('title')</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-		{{ Asset::styles() }}
-	</head>
-	<body>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width">
 
-		<div class="container">
+	{{ Asset::styles() }}
+</head>
+<body>
 
-				<header></header>
+	<div class="container" id="wrapper">
+			<header></header>
 
-				<div class="row">
-					<div class="span3" id="menu">
-						<br style="clear: both">
-						@include('layout.menu')
-					</div>
-					<div class="span9" id="content">
-						@yield('content')
-					</div>
+			<div class="row">
+				<div class="span3" id="menu">
+					<br style="clear: both">
+					@include('layout.menu')
 				</div>
+				<div class="span9" id="content">
+					@yield('content')
+				</div>
+			</div>
 
-				<footer>
-					&copy; CDZForever 2003-2012<br />Imagens e Personagens copyright &copy; Masami Kurumada / Shueisha, Toei Animation. Todos os direitos reservados.
-				</footer>
+			<footer>
+				&copy; CDZForever 2003-2012<br />Imagens e Personagens copyright &copy; Masami Kurumada / Shueisha, Toei Animation. Todos os direitos reservados.
+			</footer>
+	</div>  
 
-			</div>	
-		</div>
 
-		{{ Asset::scripts() }}
+{{ Asset::scripts() }}
 
-		{{ Anbu::render() }}
-	</body>
+{{ Anbu::render() }}
+</body>
 </html>
-
-
-
--- application/views/news/index.blade.php
+++ application/views/news/index.blade.php
@@ -6,21 +6,20 @@
 	<p>{{ __('Seja um Cavaleiro, lute por seu Deus, faça parte do CDZForever!') }}</p>
 	<p><a href="http://laravel/register" class="btn btn-primary btn-large">Cadastro</a> <a href="http://laravel/login" class="btn btn-success btn-large">Login</a></p>
 </div>
+
 <div class="row" style="margin: -10px auto">		
-	<div class="span4" >
+	<div class="span3" >
 				<h2 class="muted">Browser Game</h2>
 				<p><span class="label label-success">Por: dvl em 18/11/2012 as 22:18</span></p>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</p>
 				<p><a class="btn" href="#"><i class="icon-ok"></i> Ler tudo &raquo;</a></p>
 	</div>
 
-	<div class="span4">
+	<div class="span3">
 				<h2 class="muted">Browser Game</h2>
 				<p><span class="label label-success">Por: dvl em 18/11/2012 as 22:18</span></p>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</p>
 				<p><a class="btn" href="#"><i class="icon-ok"></i> Ler tudo &raquo;</a></p>
 	</div>
-
 </div>
-
 @endsection
